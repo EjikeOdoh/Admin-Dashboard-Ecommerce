02e536ebff9b0dba6fb4e4d008a09656417c11e5
Ejike <79414070+EjikeOdoh@users.noreply.github.com>
2022-08-06 20:55:07 +0100

    Admin dashboard first version

---
+++ orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85622',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '34567',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '12431',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with camera drone',
+        productNumber: '85622',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85622',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+
+]
